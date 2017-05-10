926b13088956599e7e11634e8ba22299b0f8d45e
Andrew Sadowski <Andrew Sadowski>
2017-05-10 17:29:40 -0400

    added database ref function that appends new elements to the train table when a child is added to the firebase database.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+  
+// firebase authentication stored in variable "config"
   var config = {
     apiKey: "AIzaSyAObfc9WyHrHS-SS5htVUZ59YotpIhCQtY",
     authDomain: "trainhw-7ada4.firebaseapp.com",
@@ -6,8 +8,10 @@
     storageBucket: "trainhw-7ada4.appspot.com",
     messagingSenderId: "497280788155"
   };
+  // initialize firebase 
   firebase.initializeApp(config);
 
+  //set up global variables
  	var database = firebase.database();
 
 	var trainName = ""
@@ -15,26 +19,108 @@
 	var trainTime = ""
 	var trainFreq = ""
 
+	// submit button function or grabbing values of input fields
 	$("#submitTrain").on("click", function(event){
+	// prevent page from refreshing 
 	event.preventDefault();
-	console.log("submit clicked!")
-
+	
+	// storing values from input fields as variables
 	trainName = $("#trainName").val().trim()
-		console.log("Train Name: " + trainName)
+		
 	trainDest = $("#trainDest").val().trim()
-		console.log("Train Destination :" + trainDest)
+		
 	trainTime = $("#trainTime").val().trim()
-		console.log("Train Time : " + trainTime)
+		
 	trainFreq = $("#trainFreq").val().trim()
-		console.log("Train Frequency : " + trainFreq)
+		
 
-
-	database.ref().set({
+	// push values and store them in firebase
+	database.ref().push({
 		
 		trainName: trainName,
 		trainDest: trainDest,
 		trainTime: trainTime,
 		trainFreq: trainFreq
-
+		// timeAdded: firebase.database.ServerValue.TIMESTAMP
 		});
-	})
+		// clear all fields with class of "input" at end of function
+		$(".input").val("")
+		return false
+	});
+
+	// function that runs when child is added to firebase database
+	database.ref().on("child_added", function(childSnapshot){
+
+		// stores the snapshot of trainName from the database
+		// and stores it in a variable
+		var trainName = childSnapshot.val().trainName
+			console.log("Train Name: " + trainName)
+		// stores the snapshot of trainDest from the database
+		// and stores it in a variable
+		var trainDest = childSnapshot.val().trainDest
+			console.log("Train Destination :" + trainDest)
+		// stores the snapshot of trainTime from the database
+		// and stores it in a variable
+		var trainTime = childSnapshot.val().trainTime
+			console.log("Train Time : " + trainTime)
+		// stores the snapshot of trainFreq from the database
+		// and stores it in a variable
+		var trainFreq = childSnapshot.val().trainFreq
+			console.log("Train Frequency : " + trainFreq)
+
+		// grabs the current time via moment.js and 
+		// stores it in a variable
+		var currentTime = moment();
+			console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+
+		// parses the string in the trainFreq variable and 
+		// retuns it as an integer, and then stores it in
+		// a variable
+		var freqInt = parseInt(trainFreq)	
+
+		// grabs the trainTime variable, pushes it back 
+		// 1 year to insure it comes becore currentTime
+		var trainTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+			console.log(trainTimeConverted)
+		
+		// grabs the trainTimeConverted variable, formats it
+		// and stores it in a new variable
+		var newTrainTime = moment(trainTimeConverted).format('HH:mm');
+			console.log("TRAIN TIME : " + trainTime);
+
+		// grabs newTrainTime variable, pushes it back 1 year
+		// to ensure it comes becore newTrainTime
+		var convertTime = moment(newTrainTime, 'HH:mm').subtract(1, 'years');
+
+		// caculates difference between the time stored in convertTime
+		// and the current time and stores it in a variable
+		var diffTime = moment().diff(moment(convertTime), 'minutes');
+			console.log("DIFFERENCE IN TIME: " + diffTime);
+		// calculates remainder between diffTime and the freqInt
+		// variable, which stores the frequency of train arivals in minutes
+		var tRemainder = diffTime % freqInt;
+			console.log("TIME REMAINING: " + tRemainder);
+		// calculates the difference beyween tRemainder and the frequency
+		// train arivals
+		var minsAway = freqInt - tRemainder;
+			console.log("MINUTES UNTIL NEXT TRAIN: " + minsAway);
+		// stores the minsAway variable and formats it to HH:mm 
+		// to receive it as an actual time on the clock 
+		var nextTrain = moment().add(minsAway, 'minutes');
+			console.log("ARRIVAL TIME: " + moment(nextTrain).format('HH:mm'));
+
+		// appends the variables to the table with the id of "trainContainer"
+		$("#trainContainer").append(
+			"<tr><td>" + trainName +
+			"</td><td>" + trainDest +
+			"</td><td>" + trainFreq +
+			"</td><td>" + moment(nextTrain).format("HH:mm") +
+			"</td><td>" + minsAway  + 
+			"</td></tr>");
+ },
+
+		function(errorObject){
+		    console.log("Read failed: " + errorObject.code)
+		});
+
+			
